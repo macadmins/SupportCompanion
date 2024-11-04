e7d529a7a5924aeeb03182f3e786b6b50811d11c
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2024-11-04 16:20:14 +0100

    Use OpenGL rendering to avoid memory leak on Metal

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ internal sealed class Program
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
-    [STAThread]
+    //[STAThread]
     public static void Main(string[] args)
     {
         BuildAvaloniaApp()
@@ -25,7 +25,12 @@ internal sealed class Program
             .UseReactiveUI()
             .With(new MacOSPlatformOptions
             {
-                ShowInDock = false, DisableDefaultApplicationMenuItems = true
+                ShowInDock = false,
+                DisableDefaultApplicationMenuItems = true
+            })
+            .With(new AvaloniaNativePlatformOptions
+            {
+                RenderingMode = new[] { AvaloniaNativeRenderingMode.OpenGl, AvaloniaNativeRenderingMode.Metal }
             });
     }
 }
