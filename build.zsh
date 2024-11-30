b6f8d05e46989cd3646a1759f440d6b107f0cb50
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2024-11-30 11:25:50 +0100

    Remove unused dependencies and build universal

--- build.zsh
+++ build.zsh
@@ -168,6 +168,8 @@ $XCODE_BUILD_PATH clean archive -scheme SupportCompanion -project "$TOOLSDIR/Sup
 CODE_SIGN_IDENTITY="$SIGNING_IDENTITY_APP" \
 OTHER_CODE_SIGN_FLAGS="--timestamp --options runtime --deep" \
 DEVELOPMENT_TEAM="$TEAM_ID" \
+ARCHS="arm64 x86_64" \
+ONLY_ACTIVE_ARCH=NO \
 -archivePath "$BUILDSDIR/SupportCompanion" >/dev/null 2>&1
 
 check_exit_code "$?" "Error running xcodebuild"
