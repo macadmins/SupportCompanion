c9125de17aa2320515565af5ffadc24009c65154
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2025-11-17 10:30:51 +0100

    Add localization strings for Jamf info and general time labels

--- SupportCompanion/Constants.swift
+++ SupportCompanion/Constants.swift
@@ -50,6 +50,8 @@ enum Constants {
         static let close = String(localized: "General.Close", defaultValue: "Close", comment: "Close")
         static let elevate = String(localized: "General.Elevate", defaultValue: "Elevate", comment: "Elevate")
         static let demote: String = String(localized: "General.Demote", defaultValue: "Demote", comment: "Demote")
+		static let ago: String = String(localized: "General.Ago", defaultValue: "Ago", comment: "Ago")
+		static let justNow: String = String(localized: "General.JustNow", defaultValue: "Just Now", comment: "Just Now")
     }
     
     enum AppPaths {
@@ -117,6 +119,7 @@ enum Constants {
         static let appPatchProgress = "ApplicationInstallProgress"
         static let battery = "Battery"
         static let pendingAppUpdates = "PendingAppUpdates"
+		static let jamfInfo = "Jamf"
     }
     
     enum CardTitle {
@@ -133,6 +136,7 @@ enum Constants {
         static let pendingUpdates = String(localized: "Card.PendingUpdatesTitle", defaultValue: "Pending Updates", comment: "Title for pending updates card")
         static let installedApps = String(localized: "Card.InstalledAppsTitle", defaultValue: "Installed Applications", comment: "Title for installed apps card")
         static let privileges = String(localized: "Card.PrivilegesTitle", defaultValue: "Privileges", comment: "Title for privileges card")
+		static let jamfInfo = "Jamf"
     }
     
     enum RebootModal {
@@ -212,6 +216,22 @@ enum Constants {
             static let serialNumber = "SerialNumber"
         }
     }
+	
+	enum JamfInfo {
+		enum Labels {
+			static let lastCheckin = String(localized: "JamfInfo.LastCheckin", defaultValue: "Check-In:", comment: "Label for the last check-in date")
+			static let lastInventory = String(localized: "JamfInfo.LastInventory", defaultValue: "Inventory:", comment: "Label for the last update date")
+			static let url = String(localized: "JamfInfo.URL", defaultValue: "URL:", comment: "Label for the URL")
+			static let id = String(localized: "JamfInfo.ID", defaultValue: "ID:", comment: "Label for the ID")
+		}
+		
+		enum Keys {
+			static let lastCheckin = "JamfLastCheckin"
+			static let lastInventory = "JamfLastInventory"
+			static let url = "JamfUrl"
+			static let id = "JamfId"
+		}
+	}
     
     enum KerberosSSO {
         enum Labels {
